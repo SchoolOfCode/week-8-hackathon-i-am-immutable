8a662f469b16343cc2bb2d401f98cb27b2f64a57
“graceoreillygit config --global user.email “graceoreilly111@gmail.com <yourname@example.comf>
2025-01-30 21:53:30 +0100

    Logo added

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
       <div className="container">
         <Routes>
           <Route path="/" element={<Homepage />} />
-          <Route path="/appliedpage" element={<AppliedPage />} />
+          <Route path="/applied" element={<AppliedPage />} />
         </Routes>
       </div>
     </>
