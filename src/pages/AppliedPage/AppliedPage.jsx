d29e813e3b74e87d3f140716c0416ec05f6f7a57
“graceoreillygit config --global user.email “graceoreilly111@gmail.com <yourname@example.comf>
2025-01-30 21:07:26 +0100

    Navigation to second page
    Used routers to navigate to Applied page
    The class of the selected page in Navbar updates dynamically

---
+++ src/pages/AppliedPage/AppliedPage.jsx
@@ -0,0 +1,13 @@
+// import Navbar from "../../components/NavBar/NavBar.jsx";
+
+
+function AppliedPage() {
+  return (
+    <>
+      <h1>Applied for Jobs</h1>
+      {/* <JobCard /> */}
+    </>
+  );
+}
+
+export default AppliedPage;
