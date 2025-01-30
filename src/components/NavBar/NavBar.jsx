1d508dfa1f5523e69f02501af60a153523c2007e
“graceoreillygit config --global user.email “graceoreilly111@gmail.com <yourname@example.comf>
2025-01-30 21:31:39 +0100

    Added logo

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -5,7 +5,7 @@ import "./NavBar.css"
 function Navbar () {
   return (
     <nav className="navbar">
-      <div className="logo">AppLogo</div>
+      <img src="/images/logo.jpeg" className="logo" alt="logo" />
       <ul className="nav-links">
         <li className="active"><NavLink to="/" className={({ isActive }) => (isActive ? "active" : "")}>Home</NavLink></li>
         <li><NavLink to="/appliedpage" className={({isActive}) => (isActive ? "active" : " ")}>Applied Jobs</NavLink></li>
